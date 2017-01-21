84a6017e4de0a862214843a83430d0738beab604
Nic Brosz <nbrosz@nicbrosz.com>
2017-01-21 10:46:03 -0600

    Add CameraFacingBillboard script based off of community script of same name: http://wiki.unity3d.com/index.php?title=CameraFacingBillboard

---
+++ Audiomancer/Assets/Scripts/CameraFacingBillboard.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFacingBillboard : MonoBehaviour {
+
+	void Update () {
+        var cameraRotation = Camera.main.transform.rotation * Vector3.forward;
+        cameraRotation.y = 0;
+        transform.LookAt(transform.position + cameraRotation, Vector3.up);
+	}
+}
