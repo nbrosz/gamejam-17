ec5bedeb5f4cd8bd75b0570988caeb2938d6cbc9
Nic Brosz <nbrosz@nicbrosz.com>
2017-01-21 15:21:20 -0600

    Add key/door mechanics.

---
+++ Audiomancer/Assets/Scripts/OnTouchCallFunction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTouchCallFunction : MonoBehaviour {
+
+    public string[] tagsToAllow;
+    public GameObject targetGameObject;
+    public string functionToCall;
+
+    void OnTriggerEnter(Collider collider) {
+        foreach (var tag in tagsToAllow) {
+            if (collider.tag.Contains(tag)) {
+                TriggerFunction();
+                break;
+            }
+        }
+    }
+
+    void TriggerFunction() {
+        if (targetGameObject != null && functionToCall != null) {
+            targetGameObject.SendMessage(functionToCall, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
---
+++ Audiomancer/Assets/Scripts/Transition.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour {
+    public Transform transitionState;
+    public float transitionTime;
+
+    private float currentTransitionTime = 0;
+    private bool transitionUp = false;
+
+    private Vector3 startPosition;
+    private Quaternion startRotation;
+    private Vector3 startScale;
+
+    void Start() {
+        startPosition = transform.position;
+        startRotation = transform.rotation;
+        startScale = transform.localScale;
+    }
+
+    void Update() {
+        if (transitionUp && currentTransitionTime < transitionTime) {
+            currentTransitionTime += Time.deltaTime;
+
+            if (currentTransitionTime > transitionTime)
+                currentTransitionTime = transitionTime;
+
+            UpdateTransition();
+        } else if (!transitionUp && currentTransitionTime > 0) {
+            currentTransitionTime -= Time.deltaTime;
+
+            if (currentTransitionTime < 0)
+                currentTransitionTime = 0;
+
+            UpdateTransition();
+        }
+    }
+
+    void UpdateTransition() {
+        var currentTime = currentTransitionTime / transitionTime;
+        transform.position = Vector3.Lerp(startPosition, transitionState.position, currentTime);
+        transform.rotation = Quaternion.Lerp(startRotation, transitionState.rotation, currentTime);
+        transform.localScale = Vector3.Lerp(startScale, transitionState.localScale, currentTime);
+    }
+
+    public void DoTransition() {
+        transitionUp = !transitionUp;
+    }
+}
