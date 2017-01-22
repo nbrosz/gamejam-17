674a93dcedb2b4dca146ee8cba991d13d25c3c77
Nic Brosz <nbrosz@nicbrosz.com>
2017-01-22 12:06:47 -0600

    Create script to randomize locations.

---
+++ Audiomancer/Assets/Scripts/RandomLocation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomLocation : MonoBehaviour {
+
+    public Transform[] randomTransforms;
+
+    void Awake() {
+        if (randomTransforms.Length > 0) {
+            // pick a random transform and set this character's position/rotation to match
+            var randomTransform = randomTransforms[Random.Range(0, randomTransforms.Length - 1)];
+            transform.position = randomTransform.position;
+            transform.rotation = randomTransform.rotation;
+        }
+    }
+}
