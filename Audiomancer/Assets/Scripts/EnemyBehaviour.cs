19bf2978f63d847a04891e2e940a54624dd7e606
Nathan Cregut <mcr6114@live.com>
2017-01-22 00:18:21 -0600

    Added more enemy things

--- Audiomancer/Assets/Scripts/EnemyBehaviour.cs
+++ Audiomancer/Assets/Scripts/EnemyBehaviour.cs
@@ -8,6 +8,7 @@ public class EnemyBehaviour : MonoBehaviour {
     public float rotateSpeed;
     public float searchTime;
     public float chargeGunTime;
+    public float stunTime;
 
     private NavMeshAgent agent;
     private Transform player;
@@ -15,94 +16,130 @@ public class EnemyBehaviour : MonoBehaviour {
     private bool searching;
     private float searchTimer;
     private float chargeGunTimer;
+    private float stunTimer;
+    private bool stunned;
     private bool ableToShoot;
+    private Health healthScript;
 
     private Quaternion lookRotation;
     private Vector3 direction;
 
+    private GameObject GOplayer;
+
 	// Use this for initialization
-	void Start () {
+	void Start() {
         agent = GetComponent<NavMeshAgent>();
         player = GameObject.FindWithTag("Player").transform;
+        ableToShoot = false;
         chasing = false;
         searching = false;
+        stunned = false;
         searchTimer = 0;
         chargeGunTimer = 0;
-        ableToShoot = false;
+        stunTimer = 0;
+        healthScript = gameObject.GetComponent<Health>();
+        GOplayer = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        // Set destination as player as long as we're chasing it
-		if (chasing) {
-            agent.SetDestination(player.position);
+	void Update() {
+        if ( GOplayer.GetComponent<Health>().Alive ) {
+            // Make sure enemy is able to do things
+            if ( healthScript.Alive ) {
+                if ( !stunned ) {
+                    // Set destination as player as long as we're chasing it
+                    if ( chasing ) {
+                        agent.SetDestination(player.position);
 
-            // Find vector pointing from current position to player
-            direction = (player.position - transform.position).normalized;
+                        // Find vector pointing from current position to player
+                        direction = (player.position - transform.position).normalized;
 
-            // Create the rotation towards player
-            lookRotation = Quaternion.LookRotation(direction);
+                        // Create the rotation towards player
+                        lookRotation = Quaternion.LookRotation(direction);
 
-            // Rotate towards lookRotation over time
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotateSpeed);
+                        // Rotate towards lookRotation over time
+                        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotateSpeed);
 
-            // Charge gun up
-            if (!ableToShoot && chargeGunTimer < chargeGunTime) {
-                chargeGunTimer += Time.deltaTime;
-                if ( chargeGunTimer >= chargeGunTime )
-                    ableToShoot = true;
+                        // Charge gun up
+                        if ( !ableToShoot && chargeGunTimer < chargeGunTime ) {
+                            chargeGunTimer += Time.deltaTime;
+                            if ( chargeGunTimer >= chargeGunTime )
+                                ableToShoot = true;
+                        }
+                    }
+
+                    // Player has left view radius but is close by, continue following for short time
+                    if ( searching ) {
+                        // Timer for searching
+                        searchTimer += Time.deltaTime;
+                        if ( searchTimer >= searchTime ) {
+                            chasing = false;
+                            searching = false;
+                            searchTimer = 0;
+                            chargeGunTimer = 0;
+                            agent.ResetPath();
+                        }
+                    }
+
+                    // Shoot the player
+                    if ( ableToShoot ) {
+                        SendMessage("DoAttack", Attack.AttackType.WeakAndWide, SendMessageOptions.DontRequireReceiver);
+                        chargeGunTimer = 0;
+                        ableToShoot = false;
+                    }
+                }
+
+                // Take time to recover from stun
+                else {
+                    stunTimer += Time.deltaTime;
+                    if ( stunTimer >= stunTime ) {
+                        stunned = false;
+                        stunTimer = 0;
+                    }
+                }
             }
         }
-
-        // Player has left view radius but is close by, continue following for short time
-        if (searching) {
-            // Timer for searching
-            searchTimer += Time.deltaTime;
-            if (searchTimer >= searchTime) {
-                chasing = false;
-                searching = false;
-                searchTimer = 0;
-                chargeGunTimer = 0;
-                agent.ResetPath();
-            }
-        }
-
-        // Shoot the player
-        if (ableToShoot) {
-            SendMessage("DoAttack", Attack.AttackType.WeakAndWide, SendMessageOptions.DontRequireReceiver);
-            chargeGunTimer = 0;
-            ableToShoot = false;
-        }
-
 	}
 
     // Check if player is within view radius
     void OnTriggerStay(Collider other) {
         if (other.tag == "Player") {    // Check for player
-            float angle = Vector3.Angle(transform.forward, other.transform.position - transform.position);
-            if (angle <= 90) {
-                chasing = true;
-                searching = false;
-                searchTimer = 0;
-            } else {
-                if (!searching && chasing)
-                    searching = true;
+            // Make sure enemy is active
+            if ( !stunned && healthScript.Alive ) {
+                float angle = Vector3.Angle(transform.forward, other.transform.position - transform.position);
+                if ( angle <= 90 ) {
+                    chasing = true;
+                    searching = false;
+                    searchTimer = 0;
+                } else {
+                    if ( !searching && chasing )
+                        searching = true;
+                }
             }
         }
     }
 
     // Check if player has left the view radius
     void OnTriggerExit(Collider other) {
-        if (other.tag == "Player") {
-            if (!searching && chasing)
+        if ( other.tag == "Player" ) {
+            // Make sure enemy is active and saw player recently
+            if ( !searching && chasing && !stunned && healthScript.Alive )
                 searching = true;
         }
     }
 
     void OnParried(AttackShot.ParriedData parriedData) {
-        if (parriedData.attacker.owner == gameObject) {
-            // stunned
+        if ( parriedData.attacker.owner == gameObject ) {
+            stunned = true;
+            agent.ResetPath();
             Debug.Log(gameObject.name + " is stunned from being parried!"); // replace
         }
+    }
+
+    void OnDamaged() {
+        // Start chasing player when shot, even if player is not seen
+        chasing = true;
+        searching = true;
+        Debug.Log(gameObject.name + " has been hurt!");
     }
 }
--- Audiomancer/Assets/Scripts/Healthbar.cs
+++ Audiomancer/Assets/Scripts/Healthbar.cs
@@ -6,11 +6,18 @@ using UnityEngine.UI;
 public class Healthbar : MonoBehaviour {
 
     public Image healthbar;
+    private Health healthScript;
 
     private float initialHealth;
     private float currentHealth;
 
+    void Start() {
+        healthScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
+        initialHealth = currentHealth = healthScript.healthPoints;
+    }
+
     void Update() {
+        currentHealth = healthScript.healthPoints;
         healthbar.fillAmount = currentHealth / initialHealth;
     }
 }
