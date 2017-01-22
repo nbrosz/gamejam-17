aec9e8ba61b82132367c3adf2c7be95f1495a0a9
DSU\jlroeder <roederjamie@gmail.com>
2017-01-21 18:15:36 -0600

    Added some cool shit

---
+++ Audiomancer/Assets/Scripts/Menu Scripts/sc_Continue.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sc_Continue : MonoBehaviour {
+    public GameObject btn_Cont;
+    public GameObject btn_Quit;
+    public GameObject btn_toMenu;
+
+	// Use this for initialization
+	void Start () {
+        Button cont = btn_Cont.GetComponent<Button>();
+        cont.onClick.AddListener(ReturnToGame);
+	}
+	
+    //Continue the Game
+    void ReturnToGame()
+    {
+        Time.timeScale = 1;
+        btn_Cont.SetActive(false);
+        btn_Quit.SetActive(false);
+        btn_toMenu.SetActive(false);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Audiomancer/Assets/Scripts/Menu Scripts/sc_start.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class sc_start : MonoBehaviour {
+    public Button btn_Start;
+
+	// Use this for initialization
+	void Start () {
+        Button start = btn_Start.GetComponent<Button>();
+        start.onClick.AddListener(StartGame);
+	}
+	
+    // Start Game
+    void StartGame()
+    {
+        SceneManager.LoadScene("TestScene");
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
