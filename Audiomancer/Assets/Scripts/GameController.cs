965a72b1f9beb396ceebfa60d69105bda6e9f902
Nic Brosz <nbrosz@nicbrosz.com>
2017-01-21 22:38:48 -0600

    Add GameController with Beat boolean to indicate when enemies can fire (and when players should fire).

---
+++ Audiomancer/Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public static bool Beat { get { return _instance.beat; } }
+
+    private static GameController _instance;
+
+    public float beatsPerMinute;
+
+    private float currentBeats = 0;
+    private bool beat = false;
+
+    void Awake() {
+        // Force singleton pattern
+        if (_instance == null)
+            _instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+	void Update () {
+        if (beat)
+            beat = false;
+
+		if (currentBeats < beatsPerMinute) {
+            currentBeats += Time.deltaTime;
+
+            if (currentBeats >= beatsPerMinute) {
+                currentBeats = 0;
+                beat = true;
+            }
+        }
+	}
+}
